4ad73494dc602f794aaab484435daf58b2a87fd3
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-01-01 00:21:39 +0900

    fix: Triangle 100%

--- src/main/java/co/wordbe/lessons/sorting/Triangle.java
+++ src/main/java/co/wordbe/lessons/sorting/Triangle.java
@@ -1,17 +1,31 @@
 package co.wordbe.lessons.sorting;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Triangle {
     public int solution(int[] A) {
-        Integer[] arr = Arrays.stream(A).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr, Collections.reverseOrder());
-        for (int i=0; i<arr.length - 2; i++) {
-            if (arr[i] < arr[i + 1] + arr[i + 2]) {
-                return 1;
-            }
+        Arrays.sort(A);
+        for (int i = 0; i < A.length - 2; i++) {
+            long P = A[i];
+            long Q = A[i + 1];
+            long R = A[i + 2];
+            if (P + Q > R) return 1;
         }
         return 0;
     }
+
+    // 변환로직이 있어서 약간의 타임아웃이 난다.
+//    public int solution(int[] A) {
+//        Long[] arr = Arrays.stream(A)
+//                .mapToLong(i -> i)
+//                .boxed()
+//                .toArray(Long[]::new);
+//        Arrays.sort(arr, Collections.reverseOrder());
+//        for (int i=0; i<arr.length - 2; i++) {
+//            if (arr[i] < arr[i + 1] + arr[i + 2]) {
+//                return 1;
+//            }
+//        }
+//        return 0;
+//    }
 }
